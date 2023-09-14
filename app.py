15e21a262f1af39d8b491b41fdc30789fad156d6
jesschannn <jessica.chan.3@stonybrook.edu>
2023-09-14 17:41:16 +0000

    added files

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+df = pd.read_csv('https://raw.githubusercontent.com/jesschannn/Azure_flask_deployment/main/HHS_Unaccompanied_Children_Program.csv')
+@app.route('/data')
+def data(data=df):
+    data = data.sample(15)
+    return render_template('data.html', data=data)
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        port=8080
+    )
